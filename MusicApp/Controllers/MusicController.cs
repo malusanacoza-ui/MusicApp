d1c9cabb3a619cb893332069c7e2c387bc3b3938
mokwelekutlwano21-lab <mokwelekutlwano21@gmail.com>
2025-12-15 11:08:35 +0200

    added a music controller, music class in models, and index.cshtml in views folder

---
+++ MusicApp/Controllers/MusicController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MusicApp.Models;
+
+
+public class MusicController : Controller
+{
+    private readonly IWebHostEnvironment _env;
+
+    public MusicController(IWebHostEnvironment env)
+    {
+        _env = env;
+    }
+
+    public IActionResult Index()
+    {
+        var musicPath = Path.Combine(_env.WebRootPath, "music");
+
+        var songs = Directory.GetFiles(musicPath, "*.mp3")
+            .Select(file => new Music
+            {
+                Title = Path.GetFileNameWithoutExtension(file),
+                FileName = Path.GetFileName(file)
+            }).ToList();
+
+        return View(songs);
+    }
+
+    public IActionResult Download(string fileName)
+    {
+        if (string.IsNullOrWhiteSpace(fileName))
+            return BadRequest();
+
+        var filePath = Path.Combine(_env.WebRootPath, "music", fileName);
+
+        if (!System.IO.File.Exists(filePath))
+            return NotFound();
+
+        return PhysicalFile(filePath, "audio/mpeg", fileName);
+    }
+
+}
---
+++ MusicApp/Models/Music.cs
@@ -0,0 +1,9 @@
+﻿namespace MusicApp.Models
+{
+    public class Music
+    {
+        public string Title { get; set; }
+        public string FileName { get; set; }
+    }
+
+}
